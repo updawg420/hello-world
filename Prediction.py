2beb3b9ce7710257443f90eabb6c621cdae7f2d1
chris <christine05789@gmail.com>
2025-03-25 08:15:30 -0700

    Dataset File

---
+++ Prediction.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+PCollege_Sal_Data = pd.read_csv('final-post-college-salaries.csv')
+PCollege_Sal_Data.head()
+
+print(PCollege_Sal_Data.shape)
