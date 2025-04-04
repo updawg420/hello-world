854f0785f0604c7e92643b6ba26d0c9304771e0e
chris <christine05789@gmail.com>
2025-04-04 14:25:24 -0700

    Adjust README

--- Prediction.py
+++ Prediction.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 PCollege_Sal_Data = pd.read_csv('final-post-college-salaries.csv')
-PCollege_Sal_Data.head()
+PCollege_Sal_Data = PCollege_Sal_Data.drop(columns=['Rank'], axis=1)
 
-print(PCollege_Sal_Data.shape)
+# print(PCollege_Sal_Data.shape)
+# list & rename columns
+# PCollege_Sal_Data.columns
+# PCollege_Sal_Data.columns = ['Rank', 'Major_code', 'Major', 'Total', 'Sample_size', 'Median', 'P25th_percentile',
+#                               'P75th_percentile', 'P90th_percentile', 'P95th_percentile', 'Unemployment_rate',
+#                               'Employment_rate']
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hello-world
-Hello, for this project I do plan on creating a predictor model utilizing machine learning. So welcome to the beginning!
+# The Project Defined
+This project will be utilizing the following dataset to train a predictive machine learning model to analyze the correlation of U.S. Post College salaries and their respective majors. Examining 763 unique people and 6 columns. 
+Link: https://www.kaggle.com/datasets/rathoddharmendra/post-college-salaries/data
