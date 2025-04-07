610506e1468b813fafb7dcf6c78dfb90eda74eca
chris <christine05789@gmail.com>
2025-04-06 17:54:10 -0700

    Data Cleaning & Preprocessing

--- Prediction.py
+++ Prediction.py
@@ -1,7 +1,16 @@
+#Importing Libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# The Salary Dataset contains 763 rows and 6 columns with the following data:
+# 1. Rank
+# 2. Major
+# 3. Degree Type
+# 4. Early Career Pay
+# 5. Mid-Career Pay
+# 6. High Meaning Percentage
 
 PCollege_Sal_Data = pd.read_csv('final-post-college-salaries.csv')
 PCollege_Sal_Data = PCollege_Sal_Data.drop(columns=['Rank'], axis=1)
